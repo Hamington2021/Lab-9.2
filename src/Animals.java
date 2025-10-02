59b1b56422b2875d4e36e4bc9845b92f76847bbf
Serena <serena.shushack@stu.crc-lennox.qc.ca>
2025-10-02 15:43:46 -0400

    Added enum and rng for a random animal

---
+++ src/Animals.java
@@ -0,0 +1,3 @@
+public enum Animals {
+    SQUIRREL, FOX, AARDVARK, PANTHER, ECHIDNA
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
-
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-                  System.out.println("i = " + i);
-        }
+        Random rand = new Random(5); // Fixed seed for reproducibility
+        Animals[] animals = Animals.values();
+        Animals selected = animals[rand.nextInt(animals.length)]; // Random selection
+        System.out.println("Selected animal: " + selected);
     }
 }
